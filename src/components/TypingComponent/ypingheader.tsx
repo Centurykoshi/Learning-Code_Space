c5feb056f08ae4168208df8169c6eb01dccb885e
Piyush Yadav <koshijeager@gmail.com>
2025-10-02 15:19:55 +0530

    17. 0 finally done

--- src/components/TypingComponent/ypingheader.tsx
+++ src/components/TypingComponent/ypingheader.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 export default function Typingheader() {
     return (
-        <div className="fixed top-5 left-0 right-0 p-4">
+        <div className="fixed top-5 left-0 right-0 p-4 z-10">
             <div className="max-w-6xl mx-auto flex justify-between items-center px-6 sm:px-8 lg:px-20">
                 <div className="flex flex-col">
                     <h1 className="text-xl font-semibold text-primary">getOkay</h1>
